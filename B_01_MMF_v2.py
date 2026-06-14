6821c601414e897d2295ba1e61424d9c348f8790
elodiekeeble <elodiekeeble@mmc.school.nz>
2026-06-15 09:28:26 +1200

    Working raffle winner (in base)

--- B_01_MMF_v2.py
+++ B_01_MMF_v2.py
@@ -1,4 +1,5 @@
 import pandas
+import random
 
 
 # Functions go here
@@ -195,6 +196,18 @@ print()
 print(f"Total Paid: ${total_paid:.2f}")
 print(f"Total Profit: ${total_profit:.2f}")
 
+# choose a random winner
+winner = random.choice(all_names)
+
+# find index of winner (ie: position in list)
+winner_index = all_names.index(winner)
+print("winner", winner, "list position", winner_index)
+
+# retrieve Total won
+total_won = mini_movie_frame.at[winner_index, 'Total']
+
+# winner announcement
+print(f"The lucky winner is {winner}. Their tick is worth ${total_won} is free!")
 
 if tickets_sold == MAX_TICKETS:
     print(f"You have sold all the tickets (ie: {MAX_TICKETS} tickets)")
